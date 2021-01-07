a7a109678db4bae06f8ce9fbcfb718de9944c338
huhuang <huhuang@ebay.com>
2021-01-06 22:57:08 -0800

    add login form

--- mailbox/src/App.js
+++ mailbox/src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
 import 'antd/dist/antd.css';
-import {Row, Col, Layout , Tag, Avatar} from 'antd';
-import {
-  CheckCircleOutlined,
-  SyncOutlined,
-  CloseCircleOutlined,
-  ExclamationCircleOutlined,
-  ClockCircleOutlined,
-  MinusCircleOutlined,
-} from '@ant-design/icons';
+import {Row, Col, Layout } from 'antd';
+
 import MailBox from "./components/email/MailBox";
 import ModelSelector from "./components/email/ModelSelector";
 import {useState} from "react";
-import Welcome from "./components/email/Welcome";
+import LoginForm from "./components/email/LoginForm";
 
 function App() {
 
@@ -24,19 +17,20 @@ function App() {
 
   const {Header, Content} = Layout;
 
+
   return (
      <Layout>
       <Header className="header">
       <Row gutter={16}>
       <Col className="gutter-row" span={6}>
-        <div><h1 style={{color:"white", "font-size":"40px", "text-align":"left"}}>Mail Tag Tool 1.0</h1></div>
+        <div><h1 style={{color:"white", "fontSize":"40px", "textAlign":"left"}}>Mail Tag Tool 1.0</h1></div>
       </Col>
       <Col className="gutter-row" span={6}>
       </Col>
       <Col className="gutter-row" span={6}>
       </Col>
       <Col className="gutter-row" span={6}>
-        <div style={{"text-align":'right'}}><Avatar size={40}>Tony</Avatar></div>
+          <LoginForm/>
       </Col>
     </Row>
     </Header>
@@ -47,9 +41,8 @@ function App() {
                      <Col span={6}><ModelSelector onModelChange={handleModelChange} /></Col>
                  </Row>
                  <Row>
-                     <Col span={24} style={{"text-align":"right"}}><MailBox currentModel={currentModel}/></Col>
+                     <Col span={24} style={{"textAlign":"right"}}><MailBox currentModel={currentModel}/></Col>
                  </Row>
-                 {/*  <Welcome/>*/}
              </div>
          </Content>
      </Layout>
---
+++ mailbox/src/components/email/LoginForm.js
@@ -0,0 +1,94 @@
+import React, { useState} from 'react';
+import { Button, Popover, Form, Input, Avatar} from 'antd';
+
+
+export default function LoginForm() {
+
+
+    const layout = {
+        labelCol: {
+            span: 8,
+        },
+        wrapperCol: {
+            span: 16,
+        },
+    };
+    const tailLayout = {
+        wrapperCol: {
+            offset: 8,
+            span: 16,
+        },
+    };
+
+    const [visible, setVisible] = useState(false);
+  //  const [visible, setVisible] = useState(false);
+
+    const hide = () => {
+        setVisible(false);
+    };
+
+    const handleVisibleChange = visible => {
+        setVisible(visible);
+    };
+
+
+    const onFinish = (values) => {
+        console.log('Success:', values);
+         hide();
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+    };
+
+    const loginForm = (
+        <Form
+            {...layout}
+            name="basic"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+        >
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+                <Input />
+            </Form.Item>
+
+            <Form.Item
+                label="Password"
+                name="password"
+                rules={[{ required: true, message: 'Please input your password!' }]}
+            >
+                <Input.Password />
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
+    );
+
+
+    const loginDialog = (<Popover
+            // content={<a onClick={hide}>Close</a>}
+            content={loginForm}
+            //  title="Title"
+            trigger="click"
+            visible={visible}
+            onVisibleChange={handleVisibleChange}
+        >
+            <Button type="primary">Login</Button>
+        </Popover>
+    )
+
+    const logoutDialog = (<Avatar size={40}>myUserName</Avatar>)
+
+
+
+    return (loginDialog)
+}
